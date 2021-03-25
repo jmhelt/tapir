daf6fde2ae16aadb67c13bc8bf2070c5e790630c
Jeffrey Helt <jmhelt13@gmail.com>
2021-03-25 11:46:22 -0400

    WIP: coordinators and nb time.

--- src/lib/configuration.h
+++ src/lib/configuration.h
@@ -32,21 +32,21 @@
 #ifndef _LIB_CONFIGURATION_H_
 #define _LIB_CONFIGURATION_H_
 
-#include "replication/common/viewstamp.h"
+#include <stdbool.h>
 
 #include <fstream>
-#include <stdbool.h>
-#include <string>
-#include <vector>
 #include <map>
 #include <set>
+#include <string>
+#include <vector>
+
+#include "replication/common/viewstamp.h"
 
 using std::string;
 
 namespace transport {
 
-struct ReplicaAddress
-{
+struct ReplicaAddress {
     string host;
     string port;
     ReplicaAddress(const string &host, const string &port);
@@ -56,39 +56,38 @@ struct ReplicaAddress
     }
 };
 
-class Configuration
-{
-public:
+class Configuration {
+   public:
     Configuration(const Configuration &c);
     Configuration(int g, int n, int f,
-                  std::map<int, std::vector<ReplicaAddress> > replicas,
+                  std::map<int, std::vector<ReplicaAddress>> replicas,
                   ReplicaAddress *multicastAddress = nullptr,
                   ReplicaAddress *fcAddress = nullptr,
-                  std::map<int, std::vector<std::string> > interfaces = std::map<int, std::vector<std::string> >());
+                  std::map<int, std::vector<std::string>> interfaces =
+                      std::map<int, std::vector<std::string>>());
     Configuration(std::istream &file);
     virtual ~Configuration();
-    ReplicaAddress replica(int group, int idx) const;
+    // ReplicaAddress replica(int group, int idx) const;
+    const ReplicaAddress &replica(int group, int idx) const;
     const ReplicaAddress *multicast() const;
     const ReplicaAddress *fc() const;
     std::string Interface(int group, int idx) const;
-    inline int GetLeaderIndex(view_t view) const {
-        return (view % n);
-    };
+    inline int GetLeaderIndex(view_t view) const { return (view % n); };
     int QuorumSize() const;
     int FastQuorumSize() const;
     bool operator==(const Configuration &other) const;
-    inline bool operator!= (const Configuration &other) const {
+    inline bool operator!=(const Configuration &other) const {
         return !(*this == other);
     }
     int replicaHost(int group, int idx) const;
     bool IsLowestGroupOnHost(int group, int idx) const;
 
-public:
-    int g;                      // number of groups
-    int n;                      // number of replicas per group
-    int f;                      // number of failures tolerated (assume homogeneous across groups)
-private:
-    std::map<int, std::vector<ReplicaAddress> > replicas;
+   public:
+    int g;  // number of groups
+    int n;  // number of replicas per group
+    int f;  // number of failures tolerated (assume homogeneous across groups)
+   private:
+    std::map<int, std::vector<ReplicaAddress>> replicas;
     std::map<int, std::map<int, int>> replicaHosts;
     std::map<int, std::map<std::string, int>> hosts;
     std::map<std::string, std::set<int>> hostToGroups;
@@ -96,39 +95,36 @@ private:
     bool hasMulticast;
     ReplicaAddress *fcAddress;
     bool hasFC;
-    std::map<int, std::vector<std::string> > interfaces;
+    std::map<int, std::vector<std::string>> interfaces;
 };
 
-}      // namespace transport
+}  // namespace transport
 
 namespace std {
-template <> struct hash<transport::ReplicaAddress>
-{
-    size_t operator()(const transport::ReplicaAddress & x) const
-        {
-            return hash<string>()(x.host) * 37 + hash<string>()(x.port);
-        }
+template <>
+struct hash<transport::ReplicaAddress> {
+    size_t operator()(const transport::ReplicaAddress &x) const {
+        return hash<string>()(x.host) * 37 + hash<string>()(x.port);
+    }
 };
-}
+}  // namespace std
 
 namespace std {
-template <> struct hash<transport::Configuration>
-{
-    size_t operator()(const transport::Configuration & x) const
-        {
-            size_t out = 0;
-            out = x.n * 37 + x.f;
-            for (int i = 0; i < x.g; i++ ) {
-                for (int j = 0; j < x.n; j++) {
-                    out *= 37;
-                    out += hash<transport::ReplicaAddress>()(x.replica(i, j));
-                }
+template <>
+struct hash<transport::Configuration> {
+    size_t operator()(const transport::Configuration &x) const {
+        size_t out = 0;
+        out = x.n * 37 + x.f;
+        for (int i = 0; i < x.g; i++) {
+            for (int j = 0; j < x.n; j++) {
+                out *= 37;
+                out += hash<transport::ReplicaAddress>()(x.replica(i, j));
             }
-            return out;
         }
+        return out;
+    }
 };
 
-}
+}  // namespace std
 
-
-#endif  /* _LIB_CONFIGURATION_H_ */
+#endif /* _LIB_CONFIGURATION_H_ */
