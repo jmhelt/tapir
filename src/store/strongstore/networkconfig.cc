c32ff3c3b46d44cafbf87b2813722ab5c0a2671b
Jeffrey Helt <jmhelt13@gmail.com>
2021-04-11 18:13:28 -0400

    remove prints

--- src/store/strongstore/networkconfig.cc
+++ src/store/strongstore/networkconfig.cc
@@ -19,15 +19,12 @@ NetworkConfiguration::~NetworkConfiguration() {}
 
 const std::string &NetworkConfiguration::GetRegion(
     const std::string &host) const {
-    Debug("GetRegion: %s", host.c_str());
-
     for (auto &kv : net_config_json_["server_regions"].items()) {
         const std::string &region = kv.key();
         const std::vector<std::string> &hosts =
             kv.value().get<std::vector<std::string>>();
         for (const std::string &h : hosts) {
             if (h == host) {
-                Debug("GetRegion: %s", region.c_str());
                 return region;
             }
         }
@@ -39,7 +36,6 @@ const std::string &NetworkConfiguration::GetRegion(
 
 const std::string &NetworkConfiguration::GetRegion(int shard_idx,
                                                    int replica_idx) const {
-    Debug("GetRegion: %d %d", shard_idx, replica_idx);
     const std::string &host =
         tport_config_.replica(shard_idx, replica_idx).host;
 
@@ -48,18 +44,15 @@ const std::string &NetworkConfiguration::GetRegion(int shard_idx,
 
 uint16_t NetworkConfiguration::GetOneWayLatency(
     const std::string &src_region, const std::string &dst_region) const {
-    Debug("GetOneWayLatency: %s %s", src_region.c_str(), dst_region.c_str());
     uint16_t rtt =
         net_config_json_["region_rtt_latencies"][src_region][dst_region]
             .get<uint16_t>();
 
-    Debug("GetOneWayLatency: %u", rtt / 2);
     return rtt / 2;
 }
 
 uint16_t NetworkConfiguration::GetMinQuorumLatency(int shard_idx,
                                                    int leader_idx) const {
-    Debug("GetMinQuorumLatency: %d %d", shard_idx, leader_idx);
     int q = tport_config_.QuorumSize();
     int n = tport_config_.n;
 
@@ -76,7 +69,6 @@ uint16_t NetworkConfiguration::GetMinQuorumLatency(int shard_idx,
 
     std::sort(lats.begin(), lats.end());
 
-    Debug("GetMinQuorumLatency: %u", lats[q - 1]);
     return lats[q - 1];
 }
 
