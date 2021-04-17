9a88dc1b6399bf588c179f06f12a5084b1bd1dd9
Jeffrey Helt <jmhelt13@gmail.com>
2021-04-17 18:23:07 -0400

    Update plotting scripts.

--- experiments/plotting/aggregate_ro_log_plot.R
+++ experiments/plotting/aggregate_ro_log_plot.R
@@ -50,11 +50,11 @@ p <- ggplot(
         y = percentile,
         color = protocol
     )) +
-    geom_line(size = 1.5) +
+    geom_line(size = 2) +
     scale_x_continuous(
-        limits = c(0, 355),
+        limits = c(0, 465),
         labels = comma,
-        breaks = pretty_breaks(n = 8),
+        breaks = pretty_breaks(n = 10),
         expand = c(0, 0),
     ) +
     scale_y_continuous(
